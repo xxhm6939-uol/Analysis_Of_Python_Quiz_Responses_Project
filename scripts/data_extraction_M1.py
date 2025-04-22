c5778c558684af05c3d9cb670d9aa5156f75211f
Taranjot Kaur <dsmf8385@leeds.ac.uk>
2025-04-22 10:24:21 +0100

    Finished M1 code

--- scripts/data_extraction_M1.py
+++ scripts/data_extraction_M1.py
@@ -1,20 +1,42 @@
+
+import os 
+
 def extract_answers_sequence(string_file_path):
+
     answers = []
 
-    with open(file_path, 'r') as file:
+    with open(string_file_path, 'r', 'utf-8') as file:
         survey = file.read()
+        current_answer = 0
+        
+        for text in survey:
+            text = survey.strip('.')
+            
+            if text.startswith('**Question'):
+                answers.append(current_answer)
+                current_answer = 0            
+     
+            elif text.startswith('[x]'):
+                try:
+                    answer_box = answer_box.split('.')
+                    answer_number = int(answer_box[1][0])
+                    answers[current_answer] = answer_number
+                except (ValueError):
+                    current_answer = 0
+        answers.append(current_answer)
+        return answers[1:]
 
-    survey_questions = survey.split('.')
-    question_number = survery_questions[0]
+list_answers = extract_answers_sequence(string_file_path)
+def write_answers_sequence(list_answers, int_n):
+    list_answers = extract_answers_sequence(string_file_path)
+    new_text_file = f"answers_list_respondent_{int_n},.txt"
 
-
-    for answer_box in survey.split('\n'):
-        answer_box = answer_box.split()
-        if '[x]' in answer_box.split():
-            answer_number = answer_box[1]
-            answers.append(answer_number)
-
-    print(f"Answers collected!")
+    with open(new_text_file, 'w') as file:
+        f.write(f"{answer}\n" for answer in list_answers)
     
+    print(f"Answers saved to text file!")
+
+
+
 
 
