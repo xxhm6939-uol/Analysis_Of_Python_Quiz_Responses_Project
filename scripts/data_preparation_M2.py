a251fbe38d53e88710a99c9d6b77ec53b9f3db84
yiran hu <hnth8628@leeds.ac.uk>
2025-04-24 18:09:02 +0100

    Finished M2 code

--- scripts/data_preparation_M2.py
+++ scripts/data_preparation_M2.py
@@ -1,1 +1,36 @@
+import os
+import urllib.request
+import shutil
+def download_answer_files(cloud_url, path_to_data_folder, respondent_index):
+        os.makedirs(path_to_data_folder, exist_ok=True)
+        for i in range(1,respondent_index + 1):
+             original_name= f"a{i}.txt"
+             download_url=f"{cloud_url}/{original_name}"
+             new_name = f"answers_respondent_{i}.txt"
+             local_path = os.path.join(path_to_data_folder, new_name)
+        try:
+         file_data = urllib.request.urlopen(url).read()
+         with open(save_path, "wb") as f:
+                f.write(file_data)
+         print(f"Downloaded: {save_as}")  
+        except:
+             
+            print(f"Failed to download a{i}.txt")
 
+        
+    
+
+
+def collate_answer_files(data_folder_path):
+    os.makedirs("output",exist_ok=True)
+    output_path = os.path.join("output","collated_answers.txt")
+    with open(output_path,'w',encoding='utf-8') as outfile:
+        for filename in sorted(os.listdir(data_folder_path)):
+            if filename.startswith("answer_respondent_")and filename.endswith(".txt"):
+                file_path = os.path.join(data_folder_path,filename)
+                with open(output_path,'r',encoding='utf-8') as outfile:
+                    outfile.write(infile.read())
+                    outfile.write("\n*\n")
+
+                    print(" Collation complete: 'collated_answers.txt' has been saved to the output/ folder.")
+                    
