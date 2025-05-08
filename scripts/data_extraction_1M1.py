af53c62a5e597690e244335cc2c61861f9eccf14
Taranjot Kaur <dsmf8385@leeds.ac.uk>
2025-05-08 16:34:18 +0100

    changed whole code

--- scripts/data_extraction_1M1.py
+++ scripts/data_extraction_1M1.py
@@ -1,37 +1,39 @@
-import os 
-
-
 
 def extract_answers_sequence(string_file_path):
 
     answers = []   # creates an empty list to store the answer values 
+    current_answer = 0 
 
     with open(string_file_path, 'r', encoding = 'utf-8') as file:
         survey = file.read()      # opens the file to read only 
-        current_answer = 0 
-
-        for text in survey: 
-            text = survey.strip('.')      # splits each line into "strips"
-
-            for i in range(len(text)):
-                if text.startswith("Question"):
-                    answers.append(current_answer)
-                    current_answer = 0   # if a line starts with "Question", current answer remains 0 
-                    question_block = text[i+1:i+5]
-
-                    for k in question_block:
-                        if text.startswith('[x]'):
-                            answer_box = answer_box.split('.')
-                            answer_number = int(answer_box[1][0])   # extracts the first digit after the fullstop (number of the answer)
-                            answers[current_answer] = answer_number   # changes current answer's value from 0 to answer number 
-                    
-            answers.append(current_answer)   # creates a list of all answer numbers
-            return answer
-
-
-
-string_file_path = r"C:\Users\user\Downloads\Analysis_Of_Python_Quiz_Responses_Project\data\answers_respondent_2.txt"
     
+    i = 0 
+
+    while i < len(survey):
+        text = survey[i].strip()
+
+        if text.startswith("Question"):
+            answers.append(current_answer)
+            current_answer = 0   # if a line starts with "Question", current answer remains 0 
+            question_block = survey[i+1:i+5]
+
+            k = 0
+
+            for k in range(4):
+                if text.startswith('[x]'):
+                    current_answer = 1 + k 
+                k += 1
+                
+            i += 5
+            
+        else:
+            i += 1
+
+    answers.append(current_answer)   # creates a list of all answer numbers
+    return answers
+
+
+
 
 list_answers = extract_answers_sequence(string_file_path)
 def write_answers_sequence(list_answers, int_n):
