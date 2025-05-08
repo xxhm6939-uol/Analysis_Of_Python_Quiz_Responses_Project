d20ecffe244665ec684fceb496d9fc6d42922a4a
Taranjot Kaur <dsmf8385@leeds.ac.uk>
2025-05-08 23:37:06 +0100

    replaced file with correct file

--- scripts/data_extraction_M1.py
+++ scripts/data_extraction_M1.py
@@ -1,36 +1,50 @@
 
-import os 
 
 
 
 def extract_answers_sequence(string_file_path):
 
     answers = []   # creates an empty list to store the answer values 
+    current_answer = 0 
+    i = 0 
 
-    with open(string_file_path, 'r', 'utf-8') as file:
-        survey = file.read()      # opens the file to read only 
-        current_answer = 0 
-
-        for text in survey: 
-            text = survey.strip('.')      # splits each line into "strips"
-
-            if text.startswith("Question"):
-                answers.append(current_answer)
-                current_answer = 0   # if a line starts with "Question", our current answer remains 0 
-
-                for text in 
-
-            elif text.startswith('[x]'):     # if line starts with "[x]", it will split again at a fullstop '.'
-                try: 
-                    answer_box = answer_box.split('.')
-                    answer_number = int(answer_box[1][0])   # extracts the first digit after the fullstop (number of the answer)
-                    answers[current_answer] = answer_number   # changes current answer's value from 0 to answer number 
-                except(ValueError):
-                    current_answer = 0   # exception ensures that if the above process fails, our default answer will remain 0 
-        
-        answers.append(current_answer)   # creates a list of all answer numbers
-        return answer[:1]
+    with open(string_file_path, 'r', encoding = 'utf-8') as file:
+        survey = file.readlines()      # opens the file to read only 
     
+    
+
+    while i < len(survey):
+        text = survey[i].strip()
+
+        if text.startswith("Question"):
+            current_answer = 0   # if a line starts with "Question", current answer remains 0 
+            question_block = survey[i+1:i+5]
+
+            k = 0
+
+            for answer_line in question_block:
+                answer_line = answer_line.strip()
+                if answer_line.startswith('[x]'):
+                    current_answer = 1 + k 
+                    break
+                k +=1
+
+            answers.append(current_answer)
+            i += 5
+    
+        else:
+            i += 1
+
+    return answers
+
+string_file_path = "data/answers_respondent_2.txt"
+
+answers = extract_answers_sequence(string_file_path)
+print(answers)
+
+
+
+
 
 list_answers = extract_answers_sequence(string_file_path)
 def write_answers_sequence(list_answers, int_n):
@@ -41,3 +55,5 @@ def write_answers_sequence(list_answers, int_n):
         f.write(f"{answer}\n" for answer in list_answers)    # sets new name to the text file containing answers list
     
     print(f"Answers saved to text file!")
+
+
