30b8eb95c2e102bff3b7c220f99c1481b733366d
hnth8628-uol <hnth8628@leeds.ac.uk>
2025-05-14 20:16:12 +0000

    add rename code

--- scripts/data_preparation_M2.py
+++ scripts/data_preparation_M2.py
@@ -55,3 +55,20 @@ def collate_answer_files(data_folder_path):
     print(" Collate function has run.")
 from data_preparation_M2 import collate_answer_files
 collate_answer_files("data")
+
+#rename to standard format
+import os
+
+folder = "data/quiz_answers_named_a1_to_a25"
+
+for i in range(1, 26):
+    old_name = f"a{i}.txt"
+    new_name = f"answers_respondent_{i}.txt"
+    old_path = os.path.join(folder, old_name)
+    new_path = os.path.join("data", new_name)
+
+    if os.path.exists(old_path):
+        os.rename(old_path, new_path)
+        print(f"Renamed {old_name} → {new_name}")
+    else:
+        print(f"{old_name} not found")
