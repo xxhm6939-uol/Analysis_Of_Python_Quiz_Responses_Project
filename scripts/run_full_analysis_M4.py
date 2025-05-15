aa272e0187858da13a4de1c377949a342724770e
xxhm6939-uol <xxhm6939@leeds.ac.uk>
2025-05-15 02:08:10 +0000

    review_by_M4

--- scripts/run_full_analysis_M4.py
+++ scripts/run_full_analysis_M4.py
@@ -1,3 +1,4 @@
+# Import needed libraries
 import os
 from data_preparation_M2 import download_answer_files, collate_answer_files
 from data_extraction_M1 import extract_answers_sequence, write_answers_sequence
@@ -13,19 +14,21 @@ def run_full_analysis():
     plot_mode = 1,2  
 
     print("="*50)
+
+    # Runs member 2's code
     download_answer_files(cloud_url, data_folder, respondent_index)
 
+    # Runs member 1's code
     for i in range(1, respondent_index + 1):
         input_file = os.path.join(data_folder, f"answers_respondent_{i}.txt")
         answers = extract_answers_sequence(input_file)
         write_answers_sequence(answers, i)
 
+    # Runs member 2's code
     collate_answer_files(structured_folder)
 
-
+    # Runs member 3's code
     visualize_data(collated_file_path, plot_mode)
-
-
 
 if __name__ == "__main__":
     run_full_analysis()
